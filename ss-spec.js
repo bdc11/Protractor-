c9917f995a69c48325d04dfdf3691a79e4064bce
Brandon Chang <brandonchang@Brandons-MacBook-Pro-2.local>
2017-12-23 15:47:06 -0800

    activated commented out tests

--- ss-spec.js
+++ ss-spec.js
@@ -19,17 +19,17 @@ describe('When testing the shopstyle website', function() {
     var colorTable = require('./pageObject/color_filters.js');
     var using = require('jasmine-data-provider');
 
-    // using(headerTable.megaheaderPick, function (headers) {
+    using(headerTable.megaheaderPick, function (headers) {
 
-    //     it ("a user should be able to click on "+headers+" megaheader", function() {
-    //         element(by.cssContainingText('a.nav-top-link', headers)).getAttribute("href").then(function(text) {
-    //             console.log(text);
-    //             ss_home_page.megaHeaders(headers);
-    //             browser.sleep(2200);
-    //             expect(browser.driver.getCurrentUrl()).toEqual(text);
-    //         });
-    //     }); 
-    // });
+        it ("a user should be able to click on "+headers+" megaheader", function() {
+            element(by.cssContainingText('a.nav-top-link', headers)).getAttribute("href").then(function(text) {
+                console.log(text);
+                ss_home_page.megaHeaders(headers);
+                browser.sleep(2200);
+                expect(browser.driver.getCurrentUrl()).toEqual(text);
+            });
+        }); 
+    });
 
     // WIP TODO
     //    using(dataTable.secondLevelHeaders, function (headers, second) {
@@ -46,37 +46,37 @@ describe('When testing the shopstyle website', function() {
     //     });
     // });
 
-    // it ("a user should be able to click on secondary headers", function() {
-    //     ss_home_page.megaHover('Women');
-    //     browser.sleep(1000);
-    //     ss_home_page.secondaryHeaders('Activewear')
-    // });
+    it ("a user should be able to click on secondary headers", function() {
+        ss_home_page.megaHover('Women');
+        browser.sleep(1000);
+        ss_home_page.secondaryHeaders('Activewear')
+    });
 
-    // it ("a user should be able to use the search bar", function() {
-    //     ss_home_page.searchBar('gucci');
-    //     element(by.css("ss-svg-icon.search-icon")).click();
-    //     ss_home_page.mouseOver();
-    // });
+    it ("a user should be able to use the search bar", function() {
+        ss_home_page.searchBar('gucci');
+        element(by.css("ss-svg-icon.search-icon")).click();
+        ss_home_page.mouseOver();
+    });
 
-    // using(colorTable.colorSideFilters, function (colors) {
+    using(colorTable.colorSideFilters, function (colors) {
 
-    //     it ("a user should be able to click on color filters", function() {
-    //         element(by.css("ss-svg-icon.search-icon")).click();
+        it ("a user should be able to click on color filters", function() {
+            element(by.css("ss-svg-icon.search-icon")).click();
 
-    //         var colorFilter = element(by.cssContainingText("h6", "Brand"));
-    //         ss_home_page.scrollTo(colorFilter); //scroll color filter into view
-    //         browser.sleep(1500);
-    //         ss_home_page.mouseOver();
-    //         ss_home_page.dismissBottomBanner();
+            var colorFilter = element(by.cssContainingText("h6", "Brand"));
+            ss_home_page.scrollTo(colorFilter); //scroll color filter into view
+            browser.sleep(1500);
+            ss_home_page.mouseOver();
+            ss_home_page.dismissBottomBanner();
 
-    //         element(by.css("label[for='"+colors+"']")).element(by.css("a")).getAttribute("href").then(function(text) {
-    //              console.log(text);
-    //              ss_home_page.colorFilter(colors);
-    //              browser.sleep(1500);
-    //              expect(browser.driver.getCurrentUrl()).toEqual(text);
-    //         });
-    //     });   
-    // });
+            element(by.css("label[for='"+colors+"']")).element(by.css("a")).getAttribute("href").then(function(text) {
+                 console.log(text);
+                 ss_home_page.colorFilter(colors);
+                 browser.sleep(1500);
+                 expect(browser.driver.getCurrentUrl()).toEqual(text);
+            });
+        });   
+    });
     
 
     it ("a user should be able to click on price filters", function() {
@@ -91,62 +91,62 @@ describe('When testing the shopstyle website', function() {
         });
     });
 
-    // it ("a user should be able to use the See All Stores button", function() {
-    //     element(by.css("ss-svg-icon.search-icon")).click();
-    //     var priceFilter = element(by.cssContainingText("h6", "Color"));
-    //     ss_home_page.scrollTo(priceFilter);
-    //     ss_home_page.seeAllButton('See All Stores');
-    //     browser.sleep(1000);
-    //     element(by.css(".search-filter-input")).isDisplayed().then(function(filter) {
-    //         expect(filter);
-    //     });
-    // });
+    it ("a user should be able to use the See All Stores button", function() {
+        element(by.css("ss-svg-icon.search-icon")).click();
+        var priceFilter = element(by.cssContainingText("h6", "Color"));
+        ss_home_page.scrollTo(priceFilter);
+        ss_home_page.seeAllButton('See All Stores');
+        browser.sleep(1000);
+        element(by.css(".search-filter-input")).isDisplayed().then(function(filter) {
+            expect(filter);
+        });
+    });
 
     it ("a user can click on cell to redirect to product page", function() {
         element(by.css("ss-svg-icon.search-icon")).click();
         ss_home_page.seeProductPage();
     });
 
-    // it ("a new user can use the Get Sale Alert from product page to favorite items", function() {
-    //     ss_home_page.megaHeaders("Men");
-    //     ss_home_page.mouseOver();
-    //     ss_home_page.seeProductPage();
-    //     element.all(by.css(".label")).get(0).click();
-    //     browser.sleep(1000);
+    it ("a new user can use the Get Sale Alert from product page to favorite items", function() {
+        ss_home_page.megaHeaders("Men");
+        ss_home_page.mouseOver();
+        ss_home_page.seeProductPage();
+        element.all(by.css(".label")).get(0).click();
+        browser.sleep(1000);
 
-    //     element(by.css("button.step-button")).isPresent().then(function(button) {
-    //         if (button == true) {
-    //             element(by.css('input.full-width.email-input')).sendKeys(""+userEmail+"");
-    //             $("button.step-button").click();
-    //             element(by.css('input.full-width.password-input')).sendKeys(""+userPassword+"");
-    //             $("button.step-button").click();
+        element(by.css("button.step-button")).isPresent().then(function(button) {
+            if (button == true) {
+                element(by.css('input.full-width.email-input')).sendKeys(""+userEmail+"");
+                $("button.step-button").click();
+                element(by.css('input.full-width.password-input')).sendKeys(""+userPassword+"");
+                $("button.step-button").click();
 
-    //         } else {
-    //             element(by.css('input.full-width.email-input')).sendKeys(""+userEmail+"");
-    //             element(by.css('input.full-width.password-input')).sendKeys(""+userPassword+"");
-    //             element(by.css("button.submit-button")).click();
-    //         }
-    //     });
-    // });
+            } else {
+                element(by.css('input.full-width.email-input')).sendKeys(""+userEmail+"");
+                element(by.css('input.full-width.password-input')).sendKeys(""+userPassword+"");
+                element(by.css("button.submit-button")).click();
+            }
+        });
+    });
 
-    // it ("a new user can use the Get Sale Alert from product cell to favorite items", function() {
-    //     ss_home_page.megaHeaders("Men");
-    //     ss_home_page.mouseOver();
-    //     element.all(by.css("div.product-cell-container")).get(0).element(by.css(".save-item-icon")).click();
-    //     element(by.css('input.full-width.email-input')).sendKeys(""+userEmail+"");
-    //     $("button.step-button").click();
-    //     element(by.css('input.full-width.password-input')).sendKeys(""+userPassword+"");
-    //     $("button.step-button").click();
-    // });
+    it ("a new user can use the Get Sale Alert from product cell to favorite items", function() {
+        ss_home_page.megaHeaders("Men");
+        ss_home_page.mouseOver();
+        element.all(by.css("div.product-cell-container")).get(0).element(by.css(".save-item-icon")).click();
+        element(by.css('input.full-width.email-input')).sendKeys(""+userEmail+"");
+        $("button.step-button").click();
+        element(by.css('input.full-width.password-input')).sendKeys(""+userPassword+"");
+        $("button.step-button").click();
+    });
 
-    // it ("a new user can sign up", function() {
-    //     element(by.buttonText("Sign Up")).click();
-    //     element(by.css('input.full-width.email-input')).sendKeys(""+userEmail+"");
-    //     element(by.css('input.full-width.password-input')).sendKeys(""+userPassword+"");
-    //     element(by.css("button.submit-button")).click();
-    //     browser.sleep(1500);
-    //     ss_home_page.checkIfLoggedIn();
-    // });
+    it ("a new user can sign up", function() {
+        element(by.buttonText("Sign Up")).click();
+        element(by.css('input.full-width.email-input')).sendKeys(""+userEmail+"");
+        element(by.css('input.full-width.password-input')).sendKeys(""+userPassword+"");
+        element(by.css("button.submit-button")).click();
+        browser.sleep(1500);
+        ss_home_page.checkIfLoggedIn();
+    });
 
     it ("a user should see that the price is the same for the product cell and product page", function() {
         ss_home_page.megaHeaders("Men");
@@ -178,11 +178,11 @@ describe('When testing the shopstyle website', function() {
         });
     });
 
-    // it ("a user can click on Buy Now from product page to redirect to retailer website", function() {
-    //     ss_home_page.megaHeaders("Men");
-    //     element(by.css("ss-svg-icon.search-icon")).click();
-    //     ss_home_page.seeProductPage();
-    //     element(by.css(".buy-button")).click();
-    //     browser.sleep(2500);
-    // });
+    it ("a user can click on Buy Now from product page to redirect to retailer website", function() {
+        ss_home_page.megaHeaders("Men");
+        element(by.css("ss-svg-icon.search-icon")).click();
+        ss_home_page.seeProductPage();
+        element(by.css(".buy-button")).click();
+        browser.sleep(2500);
+    });
 });
